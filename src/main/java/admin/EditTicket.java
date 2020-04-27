7b21f1b52c01a17fe78a21cb0517b931e4132a4b
Maciej Korcz <maciekorcz@gmail.com>
2020-04-27 17:57:47 +0200

    m

--- src/main/java/admin/EditTicket.java
+++ src/main/java/admin/EditTicket.java
@@ -14,14 +14,14 @@ import java.io.IOException;
 public class EditTicket extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        String productId;
+        String ticket_id;
         if(request.getParameter("productId")!=null) {
-            productId = request.getParameter("productId");
+            ticket_id = request.getParameter("productId");
         } else {
-            productId = (String) request.getAttribute("productId");
+            ticket_id = (String) request.getAttribute("productId");
         }
-        if(TicketDAO.checkIfProductExists(productId)){
-            Ticket singleTicket = TicketDAO.getSingleProductData(productId);
+        if(TicketDAO.checkIfProductExists(ticket_id)){
+            Ticket singleTicket = TicketDAO.getSingleProductData(ticket_id);
             request.setAttribute("singleTicket", singleTicket);
         }
 
@@ -30,39 +30,24 @@ public class EditTicket extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        String product_id = request.getParameter("productId");
-        request.setAttribute("productId", product_id);
-        String product_name = request.getParameter("product_name");
-        String category = request.getParameter("category");
-        String quantity = request.getParameter("quantity");
-        String quantity_sold = request.getParameter("quantity_sold");
-        String price = request.getParameter("price");
-        String sale_price = request.getParameter("sale_price");
-        String date_added = request.getParameter("date_added");
+        int ticket_id = Integer.parseInt(request.getParameter("ticket_id"));
+        request.setAttribute("ticket_id", ticket_id);
+        int project_id = Integer.parseInt(request.getParameter("projekt_id"));
+        String status = request.getParameter("status");
+        String title = request.getParameter("title");
         String description = request.getParameter("description");
-        String photoLinkOne = request.getParameter("photoLinkOne");
-        String photoLinkTwo = request.getParameter("photoLinkTwo");
-        String photoLinkThree = request.getParameter("photoLinkThree");
-        String photoLinkFour = request.getParameter("photoLinkFour");
-        String featured = request.getParameter("featured");
 
-        if(product_id == null){ request.setAttribute("msg", "Nie rozpoznano id edytowanego produktu. Spróbuj ponownie wyszukać produkt " +
-                "w menadżerze produktów i zedytuj jego dane jeszcze raz.");
-        } else if(product_name == null){ request.setAttribute("msg", "Nie podano nazwy produktu");
-        } else if(category == null){ request.setAttribute("msg", "Nie podano kategorii produktu");
-        } else if(quantity == null){ request.setAttribute("msg", "Nie podano ilości produktów na stanie");
-        } else if(quantity_sold == null){ request.setAttribute("msg", "Nie podano ilości sprzedanych produktów");
-        } else if(sale_price == null){ request.setAttribute("msg", "Nie podano ilości sprzedanych produktów");
-        } else if(date_added == null){ request.setAttribute("msg", "Nie podano daty dodania produktu");
-        } else if(price == null){ request.setAttribute("msg", "Nie podano ceny produktu");
-        } else if(description == null){ request.setAttribute("msg", "Nie podano opisu produktu");
+        if(ticket_id == -1){ request.setAttribute("msg", "Nie rozpoznano id ticketu");
+        } else if(project_id == -1){ request.setAttribute("msg", "Nie przypisano ticketu do projektu");
+        } else if(status == null){ request.setAttribute("msg", "Nie podano statusu projektu");
+        } else if(title == null){ request.setAttribute("msg", "Nie podano ilości sprzedanyc");
+        } else if(description == null){ request.setAttribute("msg", "Nie podano opisu ticketu");
         } else {
-            boolean done = TicketDAO.editGivenProduct(product_id, product_name, category, quantity, quantity_sold, sale_price, date_added, price,
-                    description, photoLinkOne, photoLinkTwo, photoLinkThree, photoLinkFour, featured);
+            boolean done = TicketDAO.editGivenProduct(ticket_id, project_id, status, title, description);
             if(done){
-                request.setAttribute("msg", "Pomyślnie zedytowano produkt");
+                request.setAttribute("msg", "Pomyślnie dodano ticket");
             } else {
-                request.setAttribute("msg", "Wystąpił problem w trakcie dodawania zedytowanych danych produktu do bazy, spróbuj ponownie, albo zweryfikuj logi serwera");
+                request.setAttribute("msg", "Wystąpił problem w trakcie dodawania ticketu do bazy, spróbuj ponownie, albo zweryfikuj logi serwera");
             }
         }
 
