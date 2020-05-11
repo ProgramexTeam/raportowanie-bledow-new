cfa58cd120b376be9ccf3197c74afd1e25701e04
Sylwester Wrzesinski <sw-afk>
2020-05-11 17:07:32 +0200

    File upload

--- src/main/java/user/AddTicket.java
+++ src/main/java/user/AddTicket.java
@@ -14,6 +14,11 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024,
         maxFileSize = 1024 * 1024 * 5,
@@ -21,8 +26,6 @@ import java.util.Date;
 @WebServlet("/user/ticket-manager/add-ticket")
 public class AddTicket extends HttpServlet {
     private static final String UPLOAD_DIRECTORY = "src\\main\\webapp\\assets\\files\\tickets\\";
-    String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
-    File uploadDir = new File(uploadPath);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
@@ -51,6 +54,36 @@ public class AddTicket extends HttpServlet {
                 request.setAttribute("msg", "Pomyślnie dodano ticket do bazy");
             } else {
                 request.setAttribute("msg", "Wystąpił problem w trakcie dodawania ticketu do bazy, spróbuj ponownie, albo zweryfikuj logi serwera");
+            }
+        }
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+
+        if (ServletFileUpload.isMultipartContent(request)) {
+            try {
+                List<FileItem> formItems = upload.parseRequest(request);
+                if (formItems != null && formItems.size() > 0) {
+                    for (FileItem item : formItems) {
+                        if (!item.isFormField()) {
+                            String fileName = new File(item.getName()).getName();
+                            String filePath = uploadPath + File.separator + fileName;
+                            File storeFile = new File(filePath);
+                            item.write(storeFile);
+                            request.setAttribute("message", "File "
+                                    + fileName + " has uploaded successfully!");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                //upload failed
+                e.printStackTrace();
             }
         }
 
